f406b3026994165a92c496160799ad55d270c589
OwaisZafar01 <owaiszafar2004@gmail.com>
2024-09-24 08:32:44 -0700

    Search Insert Position

---
+++ Binary Search/35. Search Insert Position.py
@@ -0,0 +1,14 @@
+def searchInsert(nums, target):
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid -1
+
+        return high + 1 
