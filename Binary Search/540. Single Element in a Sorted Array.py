7ff1e6534a002b43fd90a58bf5dbd2f301317f79
OwaisZafar01 <owaiszafar2004@gmail.com>
2024-10-21 04:45:07 -0700

    Single Element in a Sorted Array

---
+++ Binary Search/540. Single Element in a Sorted Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNonDuplicate(self, nums):
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            mid = (left + right)//2
+            if mid % 2 == 0:
+                if nums[mid] == nums[mid+1]:
+                    left = mid + 2
+                else:
+                    right = mid 
+
+            else:
+                if nums[mid] == nums[mid-1]:
+                    left = mid + 1
+                else:
+                    right = mid
+
+        return nums[left]
