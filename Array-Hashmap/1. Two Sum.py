4613399a80de10549e43a8731335532e95f3b825
Muhammad Owais Zafar <160116972+OwaisZafar01@users.noreply.github.com>
2024-11-03 01:18:00 -0700

    Two Sum

--- Array-Hashmap/1. Two Sum.py
+++ Array-Hashmap/1. Two Sum.py
@@ -1,10 +1,11 @@
 def twoSum(nums,target) :
     prevMap = {}
+    
     for i,n in enumerate(nums):
         diff = target - n 
         if diff in prevMap:
             return [prevMap[diff], i]
         prevMap[n] = i
-    
+
     #If Not Found
     return False
